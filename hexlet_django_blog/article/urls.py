6a7e5fdce7fa65870a8e474d171be4b7555bb33d
Mickle <putilovms@gmail.com>
2024-08-24 12:45:10 +0400

    gitignore

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,5 +3,6 @@ from hexlet_django_blog.article import views
 
 urlpatterns = [
     path('<str:tags>/<int:article_id>', views.ArticleView.as_view(), name='article_id'),
+    path('<int:article_id>/', views.article),
     path('', views.index),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from django.views.generic.base import View
+from django.http import Http404
+from django.views.decorators.http import require_http_methods
+
+articles = [
+    {'id': 1, 'title': '"How to foo?"', 'author': 'F. BarBaz'},
+    {'id': 2, 'title': '"Force 101"', 'author': 'O-W. Kenobi'},
+    {'id': 3, 'title': '"Top 10 skyscrapers"', 'author': 'K. Kong'},
+    {'id': 4, 'title': '"Top 10 skyscrapers (jp. edition)"', 'author': 'K. Godzilla'},
+    {'id': 5, 'title': '"5 min recepies"', 'author': 'H. Lector'},
+]
 
 
 class ArticleView(View):
@@ -13,3 +23,11 @@ class ArticleView(View):
 
 def index(request):
     return redirect(reverse('article_id', kwargs={'tags': 'python', 'article_id': 42}))
+
+
+@require_http_methods(['GET'])
+def article(request, article_id):
+    for a in articles:
+        if a['id'] == int(article_id):
+            return render(request, 'articles/article.html', context={'article': a})
+    raise Http404()
